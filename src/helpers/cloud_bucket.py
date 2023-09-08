f55e11e4c7b9b25692d33edb59f52211ba784661
ishandhanani <ishandhanani@gmail.com>
2023-09-08 16:16:41 -0400

    black formatting

--- src/helpers/cloud_bucket.py
+++ src/helpers/cloud_bucket.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from transformers import TrainingArguments
 
+
 class CloudBucket(ABC):
     @abstractmethod
     def pull_checkpoints_from_cloud(training_args: TrainingArguments) -> bool:
