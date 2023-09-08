33fd7832ad6066f0a9c93ebe892788b46e1ad70c
ishandhanani <82981111+ishandhanani@users.noreply.github.com>
2023-09-08 16:21:00 -0400

    feat: adding checkpointing functionality  (#3)
    * adding all dataclasses and parser
    
    * updated trainer format
    
    * refactored finetuning script
    
    * updated Dockerfile
    
    * rough checkpointing outline
    
    * addressing toms old commit
    
    * adding ckpt capabilities using storj
    
    * quickfixes
    
    * remove local checkpoitn folder
    
    * cleaning storage connection and adding in bucket check for ckpt
    
    * fixed checkpointing logic
    
    * remove max_steps for num_train_epochs
    
    * remove unused import, change storj import, bucket logic
    
    * adding typing import
    
    * black formatting
    
    ---------
    
    Co-authored-by: Tom Montfort <tjmontfort12@gmail.com>

--- src/training/finetune.py
+++ src/training/finetune.py
@@ -6,6 +6,7 @@ import os
 
 import torch
 from datasets import load_dataset
+from huggingface_hub import login
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
@@ -14,14 +15,16 @@ from transformers import (
     HfArgumentParser,
     Trainer,
     TrainingArguments,
+    TrainerCallback
 )
 from peft import (
     LoraConfig,
-    AutoPeftModelForCausalLM,
-    get_peft_model
+    AutoPeftModelForCausalLM
 )
+from transformers.trainer_callback import TrainerControl, TrainerState
+from transformers.training_args import TrainingArguments
 from trl import SFTTrainer
-from huggingface_hub import login
+from helpers.storj import Storj
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -32,19 +35,39 @@ class ModelArguments:
         default="meta-llama/Llama-2-7b-hf",
         metadata={"help": "The model that you want to train from Huggingface. Defaults to Meta's Llama2 7B-chat and requires a HF login"}
     )
+    new_model_name: Optional[str] = field(
+        default="agora-llama-7b-chat",
+        metadata={"help": "The name for your fine-tuned model"}
+    )
 
 @dataclass
 class DataArguments:
-    hf_data_set: str = field(
+    hf_data_path: str = field(
         default="iamtarun/python_code_instructions_18k_alpaca",
         metadata={"help": "The path to the HF dataset. Defaults to `iamtarun/python_code_instructions_18k_alpaca`"}
+    )
+    split: Optional[str] = field(
+        default="train", #TODO: should this be default?,
+        metadata={"help": "Which portion of the dataset you want to use"}
+    )
+    personal_data: Optional[str] = field(
+        default=None,
+        metadata={"help": "The path to your proprietary data"}
     )
 
 @dataclass
 class ModelTrainingArguments(TrainingArguments):
     # Specify an additional cache dir for files downloaded during training
     # Usually things are downloaded into ~/.cache/huggingface
-    # Adding this is helpful for distributed training where all workers should read from a central cache 
+    # Adding this is helpful for distributed training where all workers should read from a central cache
+    job_id : int = field(
+        default=None,
+        metadata={"help": "Unique id for the model training job"}
+    )
+    bucket_name : Optional[str] = field(
+        default=None,
+        metadata={"help": "The name of the Storj bucket to upload/download checkpoints to and from"}
+    )
     cache_dir : Optional[str] = field(
         default=None,
         metadata={"help": "Optional path where you want model checkpoints and final model to be saved"}
@@ -69,7 +92,7 @@ class ModelTrainingArguments(TrainingArguments):
         default=False,
         metadata={"help": "Enable bf16 training. Only possible on A100 GPUs"}
     )
-    per_device_train_batch_size : bool = field(
+    per_device_train_batch_size : int = field(
         default=10,
         metadata={"help": "Training batch size per device"}
     )
@@ -113,6 +136,10 @@ class ModelTrainingArguments(TrainingArguments):
         default=25,
         metadata={"help": "Save checkpoint every X updates steps"}
     )
+    save_total_limit: int = field(
+        default=2,
+        metadata={}
+    )
     logging_steps : int = field(
         default=25,
         metadata={"help": "Log every X updates steps"}
@@ -132,7 +159,8 @@ class ModelTrainingArguments(TrainingArguments):
 
 
 @dataclass
-class QuantizationArguments():
+class QuanitzationArguments():
+    # added all the params here in order to specify defaults
     load_in_4bit: bool = field(
         default=True,
         metadata={"help": "Load a model in 4bit"}
@@ -152,45 +180,59 @@ class QuantizationArguments():
 
 @dataclass
 class QloraArguments():
-    r : int = field(
+    # added all the params here in order to specify defaults
+    lora_r: Optional[int] = field(
         default=64, 
         metadata={"help": "LoRA attention dimension"}
     )
-    lora_alpha : int = field(
+    lora_alpha: Optional[int] = field(
         default=16, 
         metadata={"help": "Alpha parameter for LoRA scaling"}
     )
-    lora_dropout : float = field(
+    lora_dropout: Optional[float] = field(
         default=0.1, 
         metadata={"help": "Dropout probability for LoRA layers"}
     )
-    bias : str = field(
+    bias: Optional[str] = field(
         default="none",
-        metadata={"help": "Dropout probability for LoRA layers"}
+        metadata={}
     )
-    task_type : str = field(
+    task_type: Optional[str] = field(
         default="CAUSAL_LM",
-        metadata={"help": "Dropout probability for LoRA layers"}
+        metadata={}
     )
 
-def load_model(model_name, bnb_config):
-    n_gpus = torch.cuda.device_count()
-    cuda_available = torch.cuda.is_available()
-    if cuda_available:
-        logger.info(f'Cuda is available for use and there are {n_gpus} GPUs')
-    else:
-        logger.info('Cuda is not available for use')
+class CheckpointCallback(TrainerCallback):
+    def __init__(self, training_args: TrainingArguments, storj: Storj) -> None:
+        self.training_args = training_args
+        self.storj = storj
+   
+    def on_save(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
+        self.storj.save_checkpoints_to_cloud(args.output_dir, state.global_step, args.job_id)
+        
+def huggingface_login():
+    try: 
+        HUGGING_FACE_TOKEN = os.environ['HUGGING_FACE_TOKEN']
+    except KeyError:
+        raise Exception('Need to pass hugging face access token as environment variable.')
 
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        quantization_config = bnb_config,
-        device_map = "auto",
-    )
+    login(token=HUGGING_FACE_TOKEN)
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=True)
-    tokenizer.pad_token = tokenizer.eos_token
+def safe_save_model_for_hf_trainer(trainer: Trainer, output_dir: str):
+    """
+    - Get model state dict containing weights at time of call
+    - Convert to CPU tensors -> reduced memory?
+    - Delete original state dict to free VRAM
+    - _save() call to save it to disk/or external storage...?
+    """
+    state_dict = trainer.model.state_dict()
+    if trainer.args.should_save:
+        cpu_state_dict = {key: value.cpu() for key, value in state_dict.items()}
+        del state_dict
+        trainer._save(output_dir, state_dict=cpu_state_dict)
 
-    return model, tokenizer 
+def preprocess_data(source, tokenizer: PreTrainedTokenizer) -> Dict:
+    return {}
 
 def print_gpu_utilization():
     nvidia_smi.nvmlInit()
@@ -201,78 +243,72 @@ def print_gpu_utilization():
         print("Device {}: {}, Memory : ({:.2f}% free): {}(total), {} (free), {} (used)".format(i, nvidia_smi.nvmlDeviceGetName(handle), 100*info.free/info.total, info.total, info.free, info.used))
     nvidia_smi.nvmlShutdown()
 
-def safe_save_model_for_hf_trainer(trainer: Trainer, output_dir: str):
-    # Get model state dict containing weights at time of call
-    # Convert to CPU tensors -> reduced memory?
-    # Delete original state dict to free VRAM
-    # _save() call to save it to disk/or external storage...?
-    state_dict = trainer.model.state_dict()
-    if trainer.args.should_save():
-        cpu_state_dict = {key: value.cpu() for key, value in state_dict.items()}
-        del state_dict
-        trainer._save(output_dir, state_dict=cpu_state_dict)
-
-def preprocess_data(source, tokenizer: PreTrainedTokenizer) -> Dict:
-    return {}
-
-def huggingface_login():
-    try: 
-        HUGGING_FACE_TOKEN = os.environ['HUGGING_FACE_TOKEN']
-    except KeyError:
-        raise Exception('Need to pass hugging face access token as environment variable.')
-
-    login(token=HUGGING_FACE_TOKEN)
-
-def finetune():
-    huggingface_login()
-    
-    parser = HfArgumentParser(
-        (ModelArguments, DataArguments, ModelTrainingArguments, QuantizationArguments, QloraArguments)
-    )
-    model_args, data_args, training_args, quant_args, qlora_args = parser.parse_args_into_dataclasses()
-
-    dataset = load_dataset(data_args.hf_data_set, split="train")
-    dataset = dataset.remove_columns(['instruction', 'input', 'output'])
-
+def build_bnb_config(quant_args) -> BitsAndBytesConfig:
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=quant_args.load_in_4bit,
         bnb_4bit_quant_type=quant_args.bnb_4bit_quant_type,
-        bnb_4bit_compute_dtype=quant_args.bnb_4bit_compute_dtype,
-        use_nested_quant=quant_args.use_nested_quant
+        bnb_4bit_compute_dtype=quant_args.bnb_4bit_compute_dtype
     )
+    return bnb_config
 
-    model, tokenizer = load_model(model_args.model_name, bnb_config)
-
-    qlora_config = LoraConfig(
-        r=qlora_args.r,
+def build_lora_config(qlora_args) -> LoraConfig:
+    peft_config = LoraConfig(
         lora_alpha=qlora_args.lora_alpha,
         lora_dropout=qlora_args.lora_dropout,
+        r=qlora_args.lora_r,
         bias=qlora_args.bias,
         task_type=qlora_args.task_type
     )
+    return peft_config
 
-    model.enable_input_require_grads()
-    model_lora = get_peft_model(model, qlora_config)
-    model_lora.print_trainable_parameters()
-    del model # storage purposes
-    logger.info(print_gpu_utilization())
+def finetune():
+    huggingface_login()
 
-    trainer = SFTTrainer(
-        model=model_lora,
-        train_dataset=dataset,
-        peft_config=qlora_config,
-        dataset_text_field="prompt",
-        max_seq_length=training_args.max_seq_length,
-        tokenizer=tokenizer,
-        args=training_args,
-        packing=training_args.packing,
+    parser = HfArgumentParser(
+        (ModelArguments, DataArguments, ModelTrainingArguments, QuanitzationArguments, QloraArguments)
+    )
+    model_args, data_args, training_args, quant_args, qlora_args, remaining = parser.parse_args_into_dataclasses(return_remaining_strings=True) #TODO: remaining and the argument were added due to a weird error on Vast
+
+    # if bucket_name is not '', check for checkpoints in user's bucket
+    resume_from_checkpoint = False
+    if training_args.bucket_name:
+        storj = Storj(training_args.bucket_name)
+        resume_from_checkpoint = storj.pull_checkpoints_from_cloud(training_args)
+
+    bnb_config = build_bnb_config(quant_args=quant_args)
+    peft_config = build_lora_config(qlora_args=qlora_args)
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_args.model_name,
+        quantization_config = bnb_config,
+        device_map = "auto",
     )
 
-    trainer.train()
+    tokenizer = AutoTokenizer.from_pretrained(model_args.model_name)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    dataset = load_dataset(data_args.hf_data_path, split=data_args.split)
+    dataset = dataset.remove_columns(['instruction', 'input', 'output']) #TODO: this is python dataset specific preprocessing. Will need to handle this inside preprocess function somehow
+
+    trainer = SFTTrainer(
+        model=model,  
+        train_dataset=dataset,
+        peft_config=peft_config,
+        dataset_text_field="prompt", #TODO: this will change based on dataset. I would add this as an optional default into DataArguments
+        max_seq_length=None,
+        tokenizer=tokenizer,
+        args=training_args,
+        packing=False,
+    )
+    if training_args.bucket_name:
+        trainer.add_callback(CheckpointCallback(training_args, storj))
+    trainer.train(resume_from_checkpoint=resume_from_checkpoint)
+    trainer.save_state() #grabbed from skypilot but need to understand state better
+    safe_save_model_for_hf_trainer(trainer=trainer,
+                                   output_dir=training_args.output_dir)
 
 if __name__ == "__main__":
     finetune()
-
 
 
 
