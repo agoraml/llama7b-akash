db61e39937583544007a9bb26d488a3f9742bd58
ishandhanani <82981111+ishandhanani@users.noreply.github.com>
2023-09-27 20:27:43 -0400

    fix broken fast chat ui (#15)
    Co-authored-by: Tom Montfort <tjmontfort12@gmail.com>

--- src/inference/inference.py
+++ src/inference/inference.py
@@ -1,28 +1,102 @@
 import torch
 import os
 from peft import AutoPeftModelForCausalLM
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, pipeline
+import gradio as gr
 from src.training.finetune import print_gpu_utilization
 
-def save_model_for_inference(model_name : str, new_model_name : str, output_dir : str):
-    # save the model
-    model = AutoPeftModelForCausalLM.from_pretrained(
-        os.path.join(output_dir, "checkpoint-final"), #this should be output directory/finalsave or whatever we put in the finetune script
-        device_map="auto", 
-        torch_dtype=torch.bfloat16,
-    )
+class InferenceUI:
+    def __init__(self, model_name: str, output_dir: str):
+        model = AutoPeftModelForCausalLM.from_pretrained(
+            os.path.join(output_dir, "checkpoint-final"), #this should be output directory/finalsave or whatever we put in the finetune script
+            device_map="auto", 
+            torch_dtype=torch.bfloat16,
+        )
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        print_gpu_utilization()
 
-    #merge and unload - keep for now
-    merged_model = model.merge_and_unload(progressbar=True)
+        llama_pipeline = pipeline(
+            "text-generation",
+            model=model,
+            torch_dtype=torch.float16,
+            device_map="auto",
+            tokenizer = tokenizer 
+        )
+        
+        self.model = model
+        self.tokenizer = tokenizer
+        self.pipeline = llama_pipeline
+        
+    def format_message(self, message: str, history: list, memory_limit: int = 3) -> str:
+        """
+        Formats the message and history for the Llama model.
 
-    output_merged_dir = os.path.join(output_dir, new_model_name)
-    os.makedirs(output_merged_dir, exist_ok=True)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    merged_model.save_pretrained(output_merged_dir)
-    tokenizer.save_pretrained(output_merged_dir)
+        Parameters:
+            message (str): Current message to send.
+            history (list): Past conversation history.
+            memory_limit (int): Limit on how many past interactions to consider.
 
-    print_gpu_utilization()
-    del model
-    del merged_model
-    torch.cuda.empty_cache()
-    print_gpu_utilization()
+        Returns:
+            str: Formatted message string
+        """
+
+        SYSTEM_PROMPT = """<s>[INST] <<SYS>>
+                You are a helpful chatbot that has been optimized for coding in Python. Limit all 
+                prose in your responses. Provide correct Python code without making up any functions at all. 
+                Make sure to add comments
+                <</SYS>>"""
+
+        # always keep len(history) <= memory_limit
+        if len(history) > memory_limit:
+            history = history[-memory_limit:]
+
+        if len(history) == 0:
+            return SYSTEM_PROMPT + f"{message} [/INST]"
+
+        formatted_message = SYSTEM_PROMPT + f"{history[0][0]} [/INST] {history[0][1]} </s>"
+
+        # Handle conversation history
+        for user_msg, model_answer in history[1:]:
+            formatted_message += f"<s>[INST] {user_msg} [/INST] {model_answer} </s>"
+
+        # Handle the current message
+        formatted_message += f"<s>[INST] {message} [/INST]"
+
+        return formatted_message
+    
+    # Generate a response from the Llama model
+    def get_llama_response(self, message: str, history: list) -> str:
+        """
+        Generates a conversational response from the Llama model.
+
+        Parameters:
+            message (str): User's input message.
+            history (list): Past conversation history.
+
+        Returns:
+            str: Generated response from the Llama model.
+        """
+        query = self.format_message(message, history)
+        response = ""
+
+        sequences = self.pipeline(
+            query,
+            do_sample=True,
+            top_k=10,
+            num_return_sequences=1,
+            eos_token_id=self.tokenizer.eos_token_id,
+            max_length=1024,
+        )
+
+        generated_text = sequences[0]['generated_text']
+        response = generated_text[len(query):]  # Remove the prompt from the output
+
+        print("Chatbot:", response.strip())
+        return response.strip()
+    
+    def launch_chat(self):
+        gr.ChatInterface(self.get_llama_response).launch(server_name='0.0.0.0') 
+
+def launch_inference(model_name : str, output_dir : str):
+    inference_ui = InferenceUI(model_name, output_dir)
+    inference_ui.launch_chat()
